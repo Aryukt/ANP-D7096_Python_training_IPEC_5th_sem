4afd09e7d7c960bf42a8314365bec881f7d9b132
Aryukt Singh <aryuktsingh@gmail.com>
2026-06-26 11:00:34 +0530

    Program to check weather it is natural number or not

---
+++ classwork/day-3 Selection_Statements_26th_June/natural_num_checker.py
@@ -0,0 +1,17 @@
+'''---------------------------------------------Natural Number Checker------------------------------------------------
+Write a prgram to check weather the program is natural or not
+'''
+#----------------------------------------------------------------------------------------------------------------------
+# Taking input from user for numbers
+num=int(input("enter a number: "))
+#checking weather the number is natural or not
+if num>0:
+    print("number is a natural number")
+else :
+    print("number is not a natural number")
+#-----------------------------------------------------------------------------------------------------------------------
+'''
+Output:
+enter a number: 4
+number is a natural number
+'''
